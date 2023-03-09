70ae9c3e0623b102b14fb0341500425918235ad1
5r33n <5r33ns@gmail.com>
2023-03-09 17:14:04 +0000

    fixed treeBeats and fuelBeats types

--- components/HeartEarth.tsx
+++ components/HeartEarth.tsx
@@ -84,8 +84,8 @@ export default function HeartEarth() {
     })
 
     async function getBeatsFromData(_data) {
-        const treeBeats = await getTreeBeats()
-        const fuelBeats = await getFuelBeats()
+        const treeBeats = (await getTreeBeats()) as object | null
+        const fuelBeats = (await getFuelBeats()) as object | null
 
         let rhythm = [0, 0, 0, 0, 0, 0, 0, 0]
 
