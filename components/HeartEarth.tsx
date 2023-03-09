0b35eceeb04617595397fbbcbf959c554a984e61
5r33n <5r33ns@gmail.com>
2023-03-09 18:06:29 +0000

    trying to fix

--- components/HeartEarth.tsx
+++ components/HeartEarth.tsx
@@ -84,8 +84,8 @@ export default function HeartEarth() {
     })
 
     async function getBeatsFromData(_data) {
-        const treeBeats: any[] = await getTreeBeats()
-        const fuelBeats: any[] = await getFuelBeats()
+        const treeBeats = await getTreeBeats()
+        const fuelBeats = await getFuelBeats()
 
         let rhythm = [0, 0, 0, 0, 0, 0, 0, 0]
 
